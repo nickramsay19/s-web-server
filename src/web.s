7e548cafa66d58343a2855678375e5632c906707
nickramsay19 <nickramsay15@gmail.com>
2025-02-14 19:21:40 +1100

    working http server!

--- src/web.s
+++ src/web.s
@@ -1,12 +1,3 @@
-# rdi - 1st arg
-# rsi - 2nd arg
-# rdx - 3rd arg
-# rcx - 4th arg
-# r9 - 5th arg
-# r10 - 6th arg
-# rsp - stack pointer
-# rbp - frame pointer
-
 .equ            SYSCALL_READ,           0
 .equ            SYSCALL_WRITE,          1
 .equ            SYSCALL_CLOSE,          3
@@ -25,7 +16,7 @@
 .equ            ADDR,                   0           # 127.0.0.1 in network byte order
 .equ            PORT,                   0x901F      # 8080 little-endian
 
-.global	listen
+.global	listen, alloc, free
 .text
 listen:
     # prologue
@@ -38,7 +29,7 @@ listen:
     movq        $SOCK_STREAM,           %rsi        # socket type
     movq        $0,                     %rdx        # protocol, leave as default
     syscall 
-    pushq       %rax    
+    pushq       %rax
 
     # bind the socket to a port 
     movq        $SYSCALL_BIND,          %rax 
@@ -54,79 +45,75 @@ listen:
     syscall 
 
     # allocate space for a response buffer
-    #movq        $255,                   %rdi        # 255 bytes
-    #call        alloc   
-    #pushq       %rax   
+    movq        $255,                   %rdi        # 255 bytes
+    call        alloc   
+    pushq       %rax                                # our pointer
 
-accept: 
     # copy data into response buffer  
-    leaq        (%rsp),                 %rbx        # get buf from stack
-    leaq        (%rbx),                 %rbx        # get the buf pointer address
-
-    movq        %rbx,                   %rdi        # dest
+    cld                                             # ensure DF is clear to ensure incrementing rdi
+    movq        %rax,                   %rdi        # dest
     leaq        stat_ok(%rip),          %rsi        # src
     movq        $16,                    %rcx        # length
-    cld                                             # ensure DF is clear (if not already)
+    rep movsb 
+    leaq        res_header(%rip),       %rsi
+    movq        $60,                    %rcx
+    rep movsb   
+    leaq        res_html(%rip),         %rsi
+    movq        $171,                   %rcx
     rep movsb 
 
-    #addq        %rcx,                   %rbx  
-
-    #movq        %rbx,                   %rdi
-    #leaq        res_header(%rip),       %rsi
-    #movq        $64,                    %rcx
-    #rep movsb   
-    #addq        %rcx,                   %rdi
-    #leaq        res_html(%rip),         %rsi
-    #movq        $171,                   %rcx
-    #rep movsb 
-
+accept: 
     # accept an incoming connection 
     movq        $SYSCALL_ACCEPT,        %rax
     movq        8(%rsp),                %rdi        # sockfd
+
     xorq        %rsi,                   %rsi        # addr = NULL
     xorq        %rdx,                   %rdx        # addrlen = NULL
     syscall 
     movq        %rax,                   %rbx        # save the request fd
 
-    
-
     # write http response   
     movq        $SYSCALL_WRITE,         %rax
     movq        %rbx,                   %rdi        # fildes
-    lea         (%rsp),                 %rsi        # buf
+    movq        (%rsp),                %rsi         # buf
     movq        $255,                   %rdx        # nbyte
     syscall 
+
+    # check for errors and close socket if any found
     cmpq        $0,                     %rax
-    jne         close   
+    jb          close   
 
     jmp         accept  
 
 close:  
     # close the socket  
     movq        $SYSCALL_CLOSE,         %rax
-    movq        (%rsp),                 %rdi
+    movq        8(%rsp),                %rdi
     syscall 
 
-    movq        $723,                   %rax
+    # free memory
+    movq        (%rsp),                 %rdi
+    movq        $255,                   %rsi
+    call        free
 
     # epilogue  
     leave                                           # restore %rbp from stack
     ret 
 
-# memory allocation method
+# custom mem allocation (since no libc)
 alloc:  
     movq        $SYSCALL_MMAP,          %rax
     movq        %rdi,                   %rsi        # length
     xorq        %rdi,                   %rdi        # addr = NULL
     movq        $3,                     %rdx        # prot = PROT_READ|PROT_WRITE = 1|2
-    movq        $0x22,                  %rcx        # flags = MAP_PRIVATE|MAP_ANONYMOUS = 2|0x20
+    movq        $0x22,                  %r10        # flags = MAP_PRIVATE|MAP_ANONYMOUS = 2|0x20
     movq        $-1,                    %r8         # fd = -1
     xorq        %r9,                    %r9         # offset = 0
     syscall
     ret
 
 free:
-    mov     $11,    %rax    # munmap syscall
+    mov     $SYSCALL_MUNMAP,            %rax
     syscall
     ret
 
@@ -141,8 +128,8 @@ stat_ok:
     .ascii      "HTTP/1.1 200 OK\n"
 
 res_header:
-    .ascii      "Content-Type: text/html; charset=utf-8\n"
-    .ascii      "Content-Language: en-US\n\n"
+    .ascii      "Content-Length: 165\n"
+    .ascii      "Content-Type: text/html; charset=utf-8\n\n"
 
 res_html:
     .asciz      "<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>A simple webpage</title></head><body><h1>Simple HTML webpage</h1><p>Hello, world!</p></body></html>\n"
